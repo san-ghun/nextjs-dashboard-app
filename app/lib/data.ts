e17c20ecce0a99d27ed3824a8f7f5ac71e5ff435
Sanghun <po4865@gmail.com>
2024-04-06 23:39:36 +0200

    refactor: Refactor data fetching of overview components with Suspense component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,6 +37,9 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching latest invoice data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -48,6 +51,9 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
+    console.log('Data fetch completed after 2 seconds.');
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
